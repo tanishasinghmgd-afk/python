f4ed29c099db573c6aa53d92ce6e33b98ea41d73
tanishasinghmgd-afk <tanishasinghmgd@gmail.com>
2026-06-24 19:46:48 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,30 @@
+print("Welcome to the world of programming!")
+#Print a Number
+print(4)
+#Next line afer print
+print("Hello Python \n")
+#Pass multiple arguments for printing
+print("hello" ,5)
+#End argument of Print Statement
+print("welcome to", end = "*")
+print()
+
+
+#Activity 2
+#Creating different variables
+x = 5
+y = "Jhon"
+print(x)
+print(y)
+
+day = "Wednesday"
+print("Today is",day)
+name = input("enter your name: ")
+print("\nHello",name, "\nWelcome to codingal")
+
+
+#Activity 3 
+import keyword
+print("Python keywords are...\n")
+print(keyword.kwlist)
+
