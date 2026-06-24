9678d8369b9b1d9fc4bf7df50596b3a604b61587
tanishasinghmgd-afk <tanishasinghmgd@gmail.com>
2026-06-24 20:00:42 +0530

    Add files via upload

---
+++ routine.py
@@ -0,0 +1,11 @@
+print("My Daily Routine")
+#Step 1: Waking Up
+print("I wake up at 7 AM")
+#Step 2: Going to school
+print("I go to school at 8 AM")
+#Step 3: Playing after school
+print("I play with my friends in the evening.")
+#Step 4: Styuding
+print("I study for 2 hours at night.")
+# Calculating total study time in a week
+print("Total study hours in a week:",2*7)
