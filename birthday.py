ffeeadd144c4ed442c01220fba8a992e411740f0
tanishasinghmgd-afk <tanishasinghmgd@gmail.com>
2026-06-25 11:11:38 +0530

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,13 @@
+print("Raj:When are all your birthdays?")
+friend1= "February 11"
+friend2 = "22 october"
+friend3= "4 August"
+friend4= "28 November"
+friend5 = "19 June" 
+
+print("Kunal:", friend1)
+print("Atharv:", friend2)
+print("Parth:", friend3)
+print("Nischay:", friend4)
+print("Laksh:", friend5)
+
