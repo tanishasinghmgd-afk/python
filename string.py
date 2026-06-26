26ed0534ba1b2a80477e1631a7d1415a0c2ec6e1
tanishasinghmgd-afk <tanishasinghmgd@gmail.com>
2026-06-26 12:44:48 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,15 @@
+#lower case string
+ram = "congratulations" 
+
+#convert string to upper case
+upper_ram = ram.upper()
+print(upper_ram)
+
+
+text = str(input("enter a string:"))
+
+#upper case string
+upper_text = text.upper()
+print("upper case of a given string is:")
+print(upper_text)
+
