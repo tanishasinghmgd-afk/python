63f8763ae366b3f119b346fe7856d69569ec8592
tanishasinghmgd-afk <tanishasinghmgd@gmail.com>
2026-06-30 19:38:29 +0530

    Add files via upload

---
+++ class5.py
@@ -0,0 +1,43 @@
+#activity 1
+"""num = 3
+if num > 0:
+    print(num, "is a positive number.")
+
+num = -1
+if num > 0:
+    print(num,"is a positive number.")
+
+#activity 2
+actual_cost = float(input("please enter the actual product price:"))
+sale_amount = float(input("please ebter the sales amount:"))
+
+if (sale_amount > actual_cost):
+    amount = sale_amount - actual_cost
+    print("total profit =", (amount))
+
+else:
+    print("no profit")"""
+
+#activity 3
+i = int(input("enter a number:"))
+if (i < 15):
+    print("i is smaller than 15")
+    print("i'm in if block")
+
+else: 
+    print("i is greater than 15")
+    print("i'm in else block")
+
+print("i'm not in if and not in else block")
+
+#activity 4
+number = int(input("enter number to check "))
+print("number to be checked :" , number)
+
+if number%2==0 : 
+    print("this is an even number")
+
+else:
+    print("this is an odd number")
+
+
