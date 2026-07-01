0b04163f6f9fb09ab52b3ce4635717703c339d9f
tanishasinghmgd-afk <tanishasinghmgd@gmail.com>
2026-07-01 14:02:55 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,12 @@
+temp = int(input("enter the temperature :"))
+
+if ( temp > 30): 
+    print("temp is suitable for wearing light clothes")
+    print("i'm in if block")
+
+else:
+    print("temp is not suitable for wearing light clothes")
+    print("i'm in else block")
+    
+        
+
