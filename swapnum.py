a4452d2cc2bc350cf97c7e73431116fa6780ea94
tanishasinghmgd-afk <tanishasinghmgd@gmail.com>
2026-07-04 19:17:02 +0530

    Add files via upload

---
+++ swapnum.py
@@ -0,0 +1,15 @@
+num1 = int(input("number 1: "))
+num2 = int(input("number 2: "))
+num3 = int(input("number 3: "))
+
+temp = num1
+a = num2
+b = num3
+c = temp
+
+print("after swapping:\n")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
+ 
