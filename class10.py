20535eddb12d27cd65c30ecfdc2bfa0b8b6b4236
tanishasinghmgd-afk <tanishasinghmgd@gmail.com>
2026-07-07 11:57:25 +0530

    Add files via upload

---
+++ class10.py
@@ -0,0 +1,40 @@
+"""for i in range(11):
+    print(i)
+
+for i in range(5,31):
+    print(i)
+
+for i in range(2,21,4):
+    print(i)
+
+#activity 1
+
+n = int(input("enter the number whose sum you want to find: "))
+
+sum = 0
+
+for i in range(1, n+1):
+    sum = sum + i
+    print("\n Sum =", sum)
+
+#activity 2
+
+string = input("please enter your own string: ")
+
+string2 = ('')
+
+for i in string:
+    #print(i)
+    string2 = i + string2
+    #print(string2)
+
+print("\n The original string =", string) 
+print("\n The reversed string =", string2)"""
+
+#activity 3
+
+for i in range(11,1,-1):
+    print(i)
+
+
+
